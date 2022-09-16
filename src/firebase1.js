534752b277529bf684f631643dc9b3356e6f88e3
GURPARKAR SINGH <gurparkarran12@gmail.com>
2022-09-16 16:11:54 -0400

    Update firebase1.js

--- src/firebase1.js
+++ src/firebase1.js
@@ -10,12 +10,12 @@ import {
 
 
   const firebaseConfig = {
-    apiKey: "AIzaSyCSmN-ek4VuYz-xkBGTK3lVVmXJofywxaI",
+    apiKey: "AIzaSyCSmN-ek4VuYz-xkBGmXJofywxaI",
     authDomain: "netflix-app-38f86.firebaseapp.com",
-    projectId: "netflix-app-38f86",
-    storageBucket: "netflix-app-38f86.appspot.com",
-    messagingSenderId: "789785680765",
-    appId: "1:789785680765:web:7c9c74a2e2ad575627ca14"
+    projectId: "netflix-af86",
+    storageBucket: "netflix-a8f86.appspot.com",
+    messagingSenderId: "789780765",
+    appId: "1:78"
   };
   
 
